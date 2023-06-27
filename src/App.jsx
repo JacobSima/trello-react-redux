bf51b2608941f0c871feaa2f419636f3cba7650d
Jacob K Sima <simajacob2011@gmail.com>
2023-06-28 00:00:44 +0200

    add updated Appjs as well

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
               <Center />
             </> 
           )
-        : (<><EmptyBoard type="add"/></>)
+        : (<><EmptyBoard /></>)
        }
         <ToastContainer />
       </div>
